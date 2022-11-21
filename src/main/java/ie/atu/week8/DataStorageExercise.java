e949fadbd322ca0649c890bdf38bd25c8fedf84e
Cillian <G00404411@atu.ie>
2022-11-21 10:56:15 +0000

    Added code for reading data for part 5

--- src/main/java/ie/atu/week8/DataStorageExercise.java
+++ src/main/java/ie/atu/week8/DataStorageExercise.java
@@ -1,6 +1,7 @@
 package ie.atu.week8;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class DataStorageExercise {
 
@@ -8,15 +9,26 @@ public class DataStorageExercise {
 
         File myFile = new File("MyFile.txt");
         System.out.println("\nMy file is located at " + myFile.getAbsolutePath());
+        Scanner inputFile = null;
 
         try{
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter a file name: ");
+            String filename = input.nextLine();
+            inputFile = new Scanner (new File(filename));
+
             FileWriter myWriter = new FileWriter(myFile, true);
             PrintWriter myPrinter = new PrintWriter(myWriter);
-            myPrinter.println("Adding a final line to the file");
+            myPrinter.println("Adding another final line to the file");
             myPrinter.close();
+
         }
         catch(IOException ex){
             ex.printStackTrace();
+        }
+        while(inputFile.hasNext()){
+            String str = inputFile.nextLine();
+            System.out.println(str);
         }
     }
 }
